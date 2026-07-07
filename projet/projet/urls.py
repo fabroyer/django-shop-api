6cd0da4720daa21e29c71566e125d18813e43bbe
Fabien Royer <royer.tech@outlook.com>
2026-07-07 23:46:15 +0200

    router and viewset

--- projet/projet/urls.py
+++ projet/projet/urls.py
@@ -14,13 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
-from shop.views import CategoryView, ProductView
+
+from shop.views import CategoryViewset, ProductViewset
+
+
+router = routers.SimpleRouter()
+router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/category/', CategoryView.as_view()),
-    path('api/product/', ProductView.as_view()),
+    path('api/', include(router.urls))
 ]
--- projet/shop/views.py
+++ projet/shop/views.py
@@ -1,21 +1,21 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from shop.models import Category, Product
 from shop.serializers import CategorySerializer, ProductSerializer
 
 
-class CategoryView(APIView):
+class CategoryViewset(ReadOnlyModelViewset):
 
-    def get(self, *args, **kwargs):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
-        return Response(serializer.data)
+    serializer_class = CategorySerializer
+
+    def get_queryset(self):
+        return Category.objects.all()
 
 
-class ProductView(APIView):
+class ProductViewset(ReadOnlyModelViewset):
 
-    def get(self, *args, **kwargs):
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
+
