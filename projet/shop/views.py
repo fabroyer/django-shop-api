b836f532813205bc4688c57c0c9d9d3d813feeb3
Fabien Royer <royer.tech@outlook.com>
2026-07-09 19:40:18 +0200

    Article viewset, serializer and router

--- projet/shop/views.py
+++ projet/shop/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from shop.models import Category, Product
-from shop.serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product, Article
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
+
+
 
 
 class CategoryViewset(ReadOnlyModelViewset):
@@ -23,3 +25,13 @@ class ProductViewset(ReadOnlyModelViewset):
             queryset = queryset.filter(category_id=category_id)
         return queryset
 
+class ArticleViewset(ReadOnlyModelViewSet):
+
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(active=True)
+        product_id = self.request.GET.get('product_id')
+        if product_id is not None:
+            queryset = queryset.filter(product_id=product_id)
+        return queryset
