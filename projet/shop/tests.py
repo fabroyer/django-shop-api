03d069e103f123b93fd91e25b5c754b52be718fd
Fabien Royer <royer.tech@outlook.com>
2026-07-13 23:57:16 +0200

    ShopAPITestCase

--- projet/shop/tests.py
+++ projet/shop/tests.py
@@ -1,15 +1,24 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from rest_framework.test import APITestCase
 
 from shop.models import Category
 
 
-class TestCategory(APITestCase):
+class ShopAPITestCase(APITestCase):
 
-    url = reverse_lazy('category-list')
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(name='Fruits', active=True)
+        Category.objects.create(name='Légumes', active=False)
+
+        cls.category_2 = Category.objects.create(name='Légumes', active=True)
 
     def format_datetime(self, value):
         return value.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+class TestCategory(ShopAPITestCase):
+
+    url = reverse_lazy('category-list')
 
     def test_list(self):
         category = Category.objects.create(name='Fruits', active=True)
@@ -24,12 +33,12 @@ class TestCategory(APITestCase):
                 'name': category.name,
                 'date_created': self.format_datetime(category.date_created),
                 'date_updated': self.format_datetime(category.date_updated)
-            }
+            } for category in [self.category, self.category_2]
         ]
         self.assertEqual(response.json(), expected)
 
     def test_create(self):
-        self.assertFalse(Category.objects.exists())
+        category_count = Category.objects.counts()
         response = self.client.post(self.url, data={'name': 'Tentative'})
         self.assertEqual(response.status_code, 405)
-        self.assertFalse(Category.objects.exists())
+        self.assertFalse(Category.objects.count(), category_count)
